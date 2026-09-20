595d503364001c2bb29cd3d25fa00364da54a878
Citrea_Lingua <citrea.lingua@gmx.net>
2026-09-20 11:18:07 +0200

    Xeno accent

---
+++ Content.Server/_Starlight/Speech/EntitySystems/XenoSpeechAccentSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using Content.Shared._Starlight.Speech.Components;
+using Content.Shared.Speech;
+
+namespace Content.Server._Starlight.Speech.EntitySystems;
+
+public sealed partial class XenoSpeechAccentSystem : EntitySystem
+{
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<XenoSpeechAccentComponent, AccentGetEvent>(OnAccent);
+    }
+
+    private void OnAccent(EntityUid uid, XenoSpeechAccentComponent component, AccentGetEvent args)
+    {
+        var message = args.Message.Text;
+
+        // s => x
+        message = Regexs().Replace(message, "$1x");
+        // S => X
+        message = RegexS().Replace(message, "$1X");
+
+        args.Message.Text = message;
+    }
+
+    [GeneratedRegex("(^|\\s)s")]
+    private static partial Regex Regexs();
+    [GeneratedRegex("(^|\\s)S")]
+    private static partial Regex RegexS();
+}
---
+++ Content.Shared/_Starlight/Speech/Components/XenoSpeechAccentComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._Starlight.Speech.Components;
+
+/// <summary>
+/// Press X to S
+/// </summary>
+[RegisterComponent]
+public sealed partial class XenoSpeechAccentComponent : Component
+{
+
+}
